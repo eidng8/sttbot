bca679793101700b95d6ef0f28f56c2e60f60bf9
jc <jc@tpages.com>
2017-01-24 16:02:28 +0800

    switch php native instead of shell

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,17 @@ $cache = "$dir/data/cache";
 
 // exec("rm -f $cache/exptmpls/*");
 
-$cmd = "find $cache/ -exec touch {} \\;";
+//$cmd = "find $cache/ -exec touch {} \\;";
 // echo $cmd;
-exec($cmd);
+//exec($cmd);
+$di = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator(
+        $cache,
+        FilesystemIterator::CURRENT_AS_PATHNAME
+    )
+);
+foreach ($di as $file) {
+    touch($file);
+}
 
 require __DIR__ . '/../vendor/autoload.php';
